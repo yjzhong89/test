d203981b40d4e246e22c0a29fcb19b2d104a6382
zhongyj <zhongyj@gmail.com>
2021-04-17 17:02:53 +0800

    add 添加leetcode练习

---
+++ src/main/java/com/zyj/test/leetcode/Test129.java
@@ -0,0 +1,8 @@
+package com.zyj.test.leetcode;
+
+/**
+ * @author zhongyj
+ * @since 2021-04-17 11:52
+ */
+public class Test129 {
+}
---
+++ src/main/java/com/zyj/test/leetcode/Test3.java
@@ -0,0 +1,8 @@
+package com.zyj.test.leetcode;
+
+/**
+ * @author zhongyj
+ * @since 2021-04-17 13:53
+ */
+public class Test3 {
+}
